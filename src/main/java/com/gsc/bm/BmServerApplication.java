9371775817d778b31b0f058b67cc604a2697b07c
gesucca <gesucca.official@gmail.com>
2021-02-25 11:21:56 +0100

    first commit

---
+++ src/main/java/com/gsc/bm/BmServerApplication.java
@@ -0,0 +1,13 @@
+package com.gsc.bm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsc/bm/BmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsc.bm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
