a497ab079967cd27ca6f648a98c97c2b07cc7d70
gesucca <gesucca.official@gmail.com>
2021-02-25 15:11:13 +0100

    set allowed origins

--- src/main/java/com/gsc/bm/conf/ws/WebSocketConfig.java
+++ src/main/java/com/gsc/bm/conf/ws/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.gsc.bm.conf.ws;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final Environment env;
+
+    @Autowired
+    public WebSocketConfig(Environment env) {
+        this.env = env;
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,7 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/v1")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins(env.getProperty("stomp.allowedOrigin"))
                 .withSockJS();
     }
 
